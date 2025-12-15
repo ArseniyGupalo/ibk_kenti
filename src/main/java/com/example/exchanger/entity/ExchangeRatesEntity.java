2e563338ed2500b5b7f9c6eede0f79876f719fea
ugly <viktor222tkachenko@gmail.com>
2025-12-15 23:27:16 +0200

    vitality

--- src/main/java/com/example/exchanger/entity/ExchangeRatesEntity.java
+++ src/main/java/com/example/exchanger/entity/ExchangeRatesEntity.java
@@ -1,10 +1,17 @@
 package com.example.exchanger.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "ExchangeRates")
 public class ExchangeRatesEntity {
+    @Id
+    private Long id;
 
 }
